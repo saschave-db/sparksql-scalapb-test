9f0829a851a079f59f4d1e42417a470081b39d8e
Sascha Vetter <109700306+saschave-db@users.noreply.github.com>
2022-11-11 14:21:19 -0800

    master set to local (#1)

--- src/main/scala/RunDemo.scala
+++ src/main/scala/RunDemo.scala
@@ -9,56 +9,67 @@ import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.rdd.RDD
-import scalapb.spark.Implicits._
 import scalapb.spark.ProtoSQL
+import java.io.FileOutputStream
+import java.io.FileInputStream
 
 object RunDemo {
 
   def main(Args: Array[String]): Unit = {
-    val spark = SparkSession.builder().appName("ScalaPB Demo").getOrCreate()
+    import org.apache.spark.sql.functions._
+    import scalapb.spark.Implicits._
+
+    // write
+    val path = "/tmp/proto_contacts"
+    val op = new FileOutputStream(path)
+    testData(0).writeDelimitedTo(op)
+    op.flush()
+    op.close()
+
+    // read in
+    val input = new FileInputStream(path)
+    val contact = Contact.parseDelimitedFrom(input).get
+    input.close()
+
+    val spark = SparkSession
+      .builder()
+      .appName("ScalaPB Demo")
+      .master("local")
+      .getOrCreate()
 
     val sc = spark.sparkContext
 
-    val personsDF: DataFrame = ProtoSQL.createDataFrame(spark, testData)
+    val personsDF: DataFrame = ProtoSQL.createDataFrame(spark, Seq(contact))
+    personsDF.show(false)
 
-    val personsDS1: Dataset[Person] = personsDF.as[Person]
+    val df_ex = personsDF
+      .select(explode(col("people")).as("expl"))
+      .select(col("expl.*"))
 
-    val personsDS2: Dataset[Person] = spark.createDataset(testData)
-
-    personsDS1.show()
-
-    personsDS2.show()
-
-    personsDF.createOrReplaceTempView("persons")
-
-    spark.sql("SELECT name, age, gender, size(addresses) FROM persons").show()
-
-    spark.sql("SELECT name, age, gender, size(addresses) FROM persons WHERE age > 30")
-      .collect
-      .foreach(println)
+    df_ex.printSchema()
+    df_ex.show(false)
   }
 
-  val testData: Seq[Person] = Seq(
-    Person().update(
-      _.name := "Joe",
-      _.age := 32,
-      _.gender := Gender.MALE),
-    Person().update(
-      _.name := "Mark",
-      _.age := 21,
-      _.gender := Gender.MALE,
-      _.addresses := Seq(
-          Address(city = Some("San Francisco"), street=Some("3rd Street"))
-      )),
-    Person().update(
-      _.name := "Steven",
-      _.gender := Gender.MALE,
-      _.addresses := Seq(
-          Address(city = Some("San Francisco"), street=Some("5th Street")),
-          Address(city = Some("Sunnyvale"), street=Some("Wolfe"))
-      )),
-    Person().update(
-      _.name := "Batya",
-      _.age := 11,
-      _.gender := Gender.FEMALE))
+  val testData: Seq[Contact] = Seq(
+    Contact().addPeople(
+      Person().update(_.name := "Joe", _.age := 32, _.gender := Gender.MALE),
+      Person().update(
+        _.name := "Mark",
+        _.age := 21,
+        _.gender := Gender.MALE,
+        _.addresses := Seq(
+          Address(city = Some("San Francisco"), street = Some("3rd Street"))
+        )
+      ),
+      Person().update(
+        _.name := "Steven",
+        _.gender := Gender.MALE,
+        _.addresses := Seq(
+          Address(city = Some("San Francisco"), street = Some("5th Street")),
+          Address(city = Some("Sunnyvale"), street = Some("Wolfe"))
+        )
+      ),
+      Person().update(_.name := "Batya", _.age := 11, _.gender := Gender.FEMALE)
+    )
+  )
 }
